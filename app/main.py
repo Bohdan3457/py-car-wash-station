06392cf5cfba9db6b36a8362fcc39ce6572cdfad
Bohdan3457 <bohdankhomenko04@gmail.com>
2025-03-12 21:31:20 +0100

    solution for main.py

--- app/main.py
+++ app/main.py
@@ -37,3 +37,5 @@ class CarWashStation:
         total_score = self.average_rating * self.count_of_ratings + new_rating
         self.count_of_ratings += 1
         self.average_rating = round(total_score / self.count_of_ratings, 1)
+
+print("Ready")
